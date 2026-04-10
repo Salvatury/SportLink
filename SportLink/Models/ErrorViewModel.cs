dedc42c9dafca93d6e2e554270b1e5888c190243
Salva <Salva@SALVA-PC>
2026-04-10 14:00:56 -0300

    Agrega gitignore para Visual Studio y .NET

---
+++ SportLink/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SportLink.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
